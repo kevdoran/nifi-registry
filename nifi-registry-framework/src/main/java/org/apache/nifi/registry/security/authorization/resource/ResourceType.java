958d28582082d92367b473350b4431464ab5e466
Kevin Doran <kdoran@apache.org>
2018-01-26 16:20:31 -0500

    WIP added ability to authorize access to actuator endpoints

--- nifi-registry-framework/src/main/java/org/apache/nifi/registry/security/authorization/resource/ResourceType.java
+++ nifi-registry-framework/src/main/java/org/apache/nifi/registry/security/authorization/resource/ResourceType.java
@@ -49,4 +49,38 @@ public enum ResourceType {
 
         return type;
     }
+
+    /**
+     * Map an arbitrary resource path to its base resource type. The base resource type is
+     * what the resource path starts with.
+     *
+     * The resourcePath arg is expected to be a string of the format:
+     *
+     * {ResourceTypeValue}/arbitrary/sub-resource/path
+     *
+     * For example:
+     *   /buckets -> ResourceType.Bucket
+     *   /buckets/bucketId -> ResourceType.Bucket
+     *   /policies/read/buckets -> ResourceType.Policy
+     *
+     * @param resourcePath the path component of a URI (not including the context path)
+     * @return the base resource type
+     */
+    public static ResourceType mapFullResourcePathToResourceType(final String resourcePath) {
+        if (resourcePath == null) {
+            throw new IllegalArgumentException("Resource path must not be null");
+        }
+
+        ResourceType type = null;
+
+        for (final ResourceType rt : values()) {
+            final String rtValue = rt.getValue();
+            if(resourcePath.equals(rtValue) || resourcePath.startsWith(rtValue + "/"))  {
+                type = rt;
+                break;
+            }
+        }
+
+        return type;
+    }
 }
